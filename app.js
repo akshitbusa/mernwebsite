08dade721b87373b907db097c652c76a567d3e07
akshitbusa <124285312+akshitbusa@users.noreply.github.com>
2023-03-20 15:04:58 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000
+require('./db/conn')
+const cookieParser = require('cookie-parser')
+
+
+app.use(cookieParser())
+
+const dotenv = require('dotenv')
+const router = require('./router/auth')
+app.use(express.json())
+app.use(router)
+app.listen(port, () => { console.log("success"); })

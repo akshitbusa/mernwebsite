e5bfe67e01bbbf7a6259a5550a9b451e5e6c27d5
akshitbusa <124285312+akshitbusa@users.noreply.github.com>
2023-03-20 15:07:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
-require('./db/conn')
+require('/conn')
 const cookieParser = require('cookie-parser')
 
 
